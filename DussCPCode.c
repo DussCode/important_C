eb7d0893e2fd5b10a3957480e2c632fafb7775ec
DussGitHub <76527574+DussCode@users.noreply.github.com>
2021-03-29 17:33:49 +0200

    Add files via upload

--- DussCPCode.c
+++ DussCPCode.c
@@ -16,11 +16,11 @@ typedef struct cpstruct
 {
     //int *arr_CP;
     int arr_CP[10];
-    int counter_prodooced;
-    int counter_consoomed;
+    int *counter_prodooced;
+    int *counter_consoomed;
 }cpstruct;
 
-static sem_t Sem_consoomer, Sem_Prodoocer;
+static sem_t Sem_Consoomer, Sem_Prodoocer;
 
 int V = 0;
 int F = 0;
@@ -28,10 +28,15 @@ int F = 0;
 void *prodoocer_chad(void *);
 void *consoomer_james(void *);
 
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
 int main() 
 {
 
-    lulz *r = malloc(sizeof(struct cpstruct));
+    cpstruct *r = malloc(sizeof(struct cpstruct));
 
     int i = 0;
     pthread_t th1, th2;
@@ -42,108 +47,156 @@ int main()
     Link with -pthread.
     */
 
-    sem_init(&Sem_Prodoocer, 0, 1);
-    sem_init(&Sem_consoomer, 0, 0);
+    sem_init (&Sem_Prodoocer, 0, 1);
+    sem_init (&Sem_Consoomer, 0, 0);
 
     //for() 
     //{
-        if (pthread_create(&th1, NULL, &prodoocer_chad, r) < 0)
+        if (pthread_create (&th1, NULL, &prodoocer_chad, r) < 0)
         {
 
-            fprintf(stderr, "pthread1_create error\n");
+            fprintf (stderr, "pthread1_create error\n");
 
             exit(1);
         }
 
-        if (pthread_create(&th2, NULL, &consoomer_james, r) < 0)
+        if (pthread_create (&th2, NULL, &consoomer_james, r) < 0)
         {
 
-            fprintf(stderr, "pthread2_create error\n");
+            fprintf (stderr, "pthread2_create error\n");
 
             exit(1);
         }
     //}
-    pthread_join(th1, NULL);
-    pthread_join(th2, NULL);
+    pthread_join (th1, NULL);
+    pthread_join (th2, NULL);
 
-    if (sem_destroy(&s1) < 0) 
+    if (sem_destroy (&Sem_Prodoocer) < 0)
+        {
+            fprintf (stderr, "sem1_destroy error\n");
+
+            exit(1);
+        }
+
+    if (sem_destroy (&Sem_Consoomer) < 0)
     {
-        fprintf(stderr, "sem1_destroy error\n");
+        fprintf (stderr, "sem2_destroy error\n");
 
-        exit(1);
-    }
-
-    if (sem_destroy(&s2) < 0)
-    {
-        fprintf(stderr, "sem2_destroy error\n");
-
-        exit(1);
+        exit (1);
     }
     return 0;
 }
-
-void *prodoocer_chad(void *arg) 
+/**
+ * @brief 
+ * 
+ * @param arg 
+ * @return void* 
+ */
+void *prodoocer_chad (void *arg) 
 {
-    int *buffer_P = (int *)arg;
-    int counting_P = (int *)arg;
+    int *buffer_P = (int *) arg;
+    int *counting_P = (int *) arg;
     
     int k = 0;
     int i = 0;
 
-    for(i = 0; i < 10; i++) 
+    for (i = 0; i < 10; i++) 
     {
-        if (buffer_P[9] == 1)
+        while (buffer_P[i] != 1) 
         {
-            sem_wait(&Sem_Prodoocer);
-            printf("WAITING PRODOOCER")
-        }
-
-        while(buffer_P[i] != 1) 
-        {
-            while (buffer_P[9] == 1)
-            {
-                sem_post(&Sem_Consoomer);
-                printf("GOING CONSOOMER")
-            }
-
             buffer_P[i] += 1;
             counting_P += 1;
-            ((cpstruct *)arg) -> arr_CP[i] = buffer_P[i];
-            ((cpstruct *)arg)-> counter_prodooced = counting_P;
+            ((cpstruct *) arg) -> arr_CP[i] = buffer_P[i];
+            ((cpstruct *) arg) -> counter_prodooced = counting_P;
+
+            if (buffer_P[9] == 1)
+            {
+                sem_wait (&Sem_Prodoocer);
+                sem_post (&Sem_Consoomer);
+                printf ("GOING CONSOOMER");
+            }
         }
     }
 
-    pthread_exit(0);
+    pthread_exit (0);
 }
-
-void *consoomer_james(void *arg) 
+/**
+ * @brief 
+ * 
+ * @param arg 
+ * @return void* 
+ */
+void *consoomer_james (void *arg) 
 {
-    int *buffer_C = (int *)arg;
-    int counting_C = (int *)arg;
+    int *buffer_C = (int *) arg;
+    int *counting_C = (int *) arg;
 
     int k = 0;
     int i = 10;
 
-    for (i = 10; i > 0; i--)
+    for (i = 9; i >=  0; i--)
     {
-        while (buffer_P[i] != 1)
+        while (buffer_C[i] != 0)
         {
-            while (buffer_P[0] == 1)
+
+            buffer_C[i] -= 1;
+            ((cpstruct *) arg) -> arr_CP[i] = buffer_C[i];
+
+            if (buffer_C[0] == 0)
             {
-                sem_wait(&Sem_Prodoocer);
-                printf("WAITING TEST_2")
+                sem_post (&Sem_Prodoocer);
+                printf ("WAITING TEST_2");
             }
 
-            buffer_P[i] -= 1;
-            ((cpstruct *)arg) -> arr_CP[i] = buffer_P[i];
-
-            if (buffer_P[9] == 1)
-            {
-                sem_post(&Sem_Consoomer);
-                printf("GOING PRODOOCER")
-            }
+            sem_wait(&Sem_Consoomer);
         }
     }
 
-    pthread_exit(0);
+    pthread_exit (0);
 }
+
+/*
+HOW TO USE SEMAPHORES
+
+To initialize a semaphore, use sem_init:
+      int sem_init(sem_t *sem, int pshared, unsigned int value);
+sem points to a semaphore object to initialize
+pshared is a flag indicating whether or not the semaphore should be shared with fork()ed processes. LinuxThreads does not currently support shared semaphores
+value is an initial value to set the semaphore to
+Example of use:
+      sem_init(&sem_name, 0, 10);
+    
+///////////////////////////////////////////////////////////
+
+To wait on a semaphore, use sem_wait:
+      int sem_wait(sem_t *sem);
+Example of use:
+      sem_wait(&sem_name); 
+If the value of the semaphore is negative, the calling process blocks; one of the blocked processes wakes up when another process calls sem_post.
+
+///////////////////////////////////////////////////////////
+
+To increment the value of a semaphore, use sem_post:
+      int sem_post(sem_t *sem);
+Example of use:
+      sem_post(&sem_name); 
+It increments the value of the semaphore and wakes up a blocked process waiting on the semaphore, if any.
+
+///////////////////////////////////////////////////////////
+
+To find out the value of a semaphore, use
+      int sem_getvalue(sem_t *sem, int *valp);
+gets the current value of sem and places it in the location pointed to by valp
+Example of use:
+      int value; 
+      sem_getvalue(&sem_name, &value); 
+      printf("The value of the semaphors is %d\n", value);
+
+///////////////////////////////////////////////////////////
+
+To destroy a semaphore, use
+      int sem_destroy(sem_t *sem);
+destroys the semaphore; no threads should be waiting on the semaphore if its destruction is to succeed.
+Example of use:
+     sem_destroy(&sem_name); 
+*/
--- DussLSCode.c
+++ DussLSCode.c
@@ -1,149 +1,150 @@
-/*
+/**
  * @file DussLSCode.c
- * @Author Dussin Francesco (you@domain.com)
- * @brief
+ * @author your name (you@domain.com)
+ * @brief 
  * @version 0.1
- * @date 2021-03-24
+ * @date 2021-03-29
+ * 
  * @copyright Copyright (c) 2021
-*/
+ * 
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <errno.h>
 
 typedef struct cpstruct
 {
     //int *arr_CP;
-    int arr_CP[10];
-    int counter_prodooced;
-    int counter_consoomed;
+    int arr_1[10];
+    int arr_2[10];
 }cpstruct;
 
-static sem_t Sem_consoomer, Sem_Prodoocer;
+sem_t Sem_Writer_Duss, Sem_Reader_Cris;
 
-int V = 0;
-int F = 0;
-
-void *prodoocer_chad(void *);
-void *consoomer_james(void *);
+void *Reader_Cris(void *);
+void *Writer_Duss(void *);
 
 int main()
 {
 
-    lulz *r = malloc(sizeof(struct cpstruct));
+    cpstruct *r = malloc(sizeof(struct cpstruct));
 
     int i = 0;
-    pthread_t th1, th2;
+    int chunk_readers = 0;
 
-    /*
-    #include <semaphore.h>
-    int sem_init(sem_t *sem, int pshared, unsigned int value);
-    Link with -pthread.
-    */
+    printf("number of th_readers : ");
+    scanf("%d", &chunk_readers);
 
-    sem_init(&Sem_Prodoocer, 0, 1);
-    sem_init(&Sem_consoomer, 0, 0);
+    pthread_t th_writer, th_readers[chunk_readers];
 
-    //for()
-    //{
-    if (pthread_create(&th1, NULL, &prodoocer_chad, r) < 0)
+    sem_init(&Sem_Writer_Duss, 0, 1);
+    sem_init(&Sem_Reader_Cris, 0, 0);
+
+    pthread_create(&th_writer, NULL, &Writer_Duss, r);
+
+    for (i = 0; i < chunk_readers; i++)
     {
-
-        fprintf(stderr, "pthread1_create error\n");
-
-        exit(1);
+        pthread_create(&th_readers[i], NULL, &Reader_Cris, r);
     }
 
-    if (pthread_create(&th2, NULL, &consoomer_james, r) < 0)
+    pthread_join(th_writer, NULL);
+
+    for (i = 0; i < chunk_readers; i++)
     {
-
-        fprintf(stderr, "pthread2_create error\n");
-
-        exit(1);
+        pthread_join(th_readers[i], NULL);
     }
-    //}
-    pthread_join(th1, NULL);
-    pthread_join(th2, NULL);
-
-    if (sem_destroy(&s1) < 0)
-    {
-        fprintf(stderr, "sem1_destroy error\n");
-
-        exit(1);
-    }
-
-    if (sem_destroy(&s2) < 0)
-    {
-        fprintf(stderr, "sem2_destroy error\n");
-
-        exit(1);
-    }
-    return 0;
-}
-
-void *prodoocer_chad(void *arg)
-{
-    int *buffer_P = (int *)arg;
-    int counting_P = (int *)arg;
-
-    int k = 0;
-    int i = 0;
 
     for (i = 0; i < 10; i++)
     {
-        if (buffer_P[9] == 1)
-        {
-            sem_wait(&Sem_Prodoocer);
-            printf("WAITING PRODOOCER")
-        }
-
-        while (buffer_P[i] != 1)
-        {
-            while (buffer_P[9] == 1)
-            {
-                sem_post(&Sem_Consoomer);
-                printf("GOING CONSOOMER")
-            }
-
-            buffer_P[i] += 1;
-            counting_P += 1;
-            ((cpstruct *)arg)->arr_CP[i] = buffer_P[i];
-            ((cpstruct *)arg)->counter_prodooced = counting_P;
-        }
+        printf(r -> arr_1[i]);
     }
 
-    pthread_exit(0);
+    sem_destroy(&Sem_Writer_Duss);
+    sem_destroy(&Sem_Reader_Cris);
+
+    return 0;
 }
 
-void *consoomer_james(void *arg)
+//////////////////////////////////////
+
+void *Reader_Cris(void *arg)
 {
-    int *buffer_C = (int *)arg;
-    int counting_C = (int *)arg;
 
-    int k = 0;
-    int i = 10;
 
-    for (i = 10; i > 0; i--)
+    pthread_exit(0);
+}
+
+//////////////////////////////////////
+
+void *Writer_Duss(void *arg)
+{
+    int *Arr_W_Function = (int *)arg;
+    int i = 0;
+    int arr_length = 10;
+
+    for(i = 0; i < 10; i++)
     {
-        while (buffer_P[i] != 1)
+        Arr_W_Function[i] += 1;
+
+        if(Arr_W_Function[10] == 1)
         {
-            while (buffer_P[0] == 1)
-            {
-                sem_wait(&Sem_Prodoocer);
-                printf("WAITING TEST_2")
-            }
-
-            buffer_P[i] -= 1;
-            ((cpstruct *)arg)->arr_CP[i] = buffer_P[i];
-
-            if (buffer_P[9] == 1)
-            {
-                sem_post(&Sem_Consoomer);
-                printf("GOING PRODOOCER")
-            }
+            sem_wait(&Sem_Writer_Duss);
+            printf("going reader");
         }
     }
 
     pthread_exit(0);
 }
+
+/*
+HOW TO USE SEMAPHORES
+
+#include <semaphore.h>
+int sem_init(sem_t *sem, int pshared, unsigned int value);
+Link with - pthread.
+
+To initialize a semaphore, use sem_init:
+      int sem_init(sem_t *sem, int pshared, unsigned int value);
+sem points to a semaphore object to initialize
+pshared is a flag indicating whether or not the semaphore should be shared with fork()ed processes. LinuxThreads does not currently support shared semaphores
+value is an initial value to set the semaphore to
+Example of use:
+      sem_init(&sem_name, 0, 10);
+    
+///////////////////////////////////////////////////////////
+
+To wait on a semaphore, use sem_wait:
+      int sem_wait(sem_t *sem);
+Example of use:
+      sem_wait(&sem_name); 
+If the value of the semaphore is negative, the calling process blocks; one of the blocked processes wakes up when another process calls sem_post.
+
+///////////////////////////////////////////////////////////
+
+To increment the value of a semaphore, use sem_post:
+      int sem_post(sem_t *sem);
+Example of use:
+      sem_post(&sem_name); 
+It increments the value of the semaphore and wakes up a blocked process waiting on the semaphore, if any.
+
+///////////////////////////////////////////////////////////
+
+To find out the value of a semaphore, use
+      int sem_getvalue(sem_t *sem, int *valp);
+gets the current value of sem and places it in the location pointed to by valp
+Example of use:
+      int value; 
+      sem_getvalue(&sem_name, &value); 
+      printf("The value of the semaphors is %d\n", value);
+
+///////////////////////////////////////////////////////////
+
+To destroy a semaphore, use
+      int sem_destroy(sem_t *sem);
+destroys the semaphore; no threads should be waiting on the semaphore if its destruction is to succeed.
+Example of use:
+     sem_destroy(&sem_name); 
+*/
